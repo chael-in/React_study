42fd1dd749cc4a1ca11642d0d4b8124ca5c91f6a
chael-in <cofls3511@gmail.com>
2025-02-07 03:28:02 +0900

    Update App.js

--- 2주차/App.js
+++ 2주차/App.js
@@ -1,49 +1,52 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import ToDoList from './ToDoList/ToDoList';
+import CreateToDo from './ToDoList/CreateToDo';
 
 function App() {
-    const [toDo, setTodo] = useState('');
-    const [toDos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
-    const onChange = (e) => {
-        setTodo(e.target.value);
-    };
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (toDo.trim() !== '') {
-            setTodos([...toDos, {text: toDo, completed: false}]);
-            setTodo('');
-        }
-    };
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const onRemove = (index) => {
-        setTodos(toDos.filter((_, i) => i !== index));
-    };
+  const [todos, setTodos] = useState(
+    [{id: 0,
+    todo: ''},
+  ]);
 
-    const onComplete = (index) => {
-        setTodos(
-            toDos.map((todo, i) => i === index ? {...todo, completed: !todo.completed} : todo)
-        );
-    };
+  const nextId = useRef(1);
 
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <form onSubmit={onSubmit}>
-                <input id='todo' type='text' value={toDo} onChange={onChange} />
-                <button id='addButton' type='submit'>✚</button>
-            </form>
-            <ul id='todoBox'>
-                {toDos.map((todo, index) => (
-                    <li key={index} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
-                        {todo.text}
-                        <button onClick={() => onComplete(index)}>완료</button>
-                        <button onClick={() => onRemove(index)}>삭제</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const onCreate = () => {
+    const todo = {
+      id: nextId.current,
+      input
+    };
+    setTodos(todos.concat(todo));
+    setInput('');
+    nextId.current += 1;
+  };
+
+  const onComplete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  };
+
+  const onRemove = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <CreateToDo 
+        input={input}
+        onChange={onChange}
+        onCreate={onCreate} />
+      <ToDoList 
+        todos={todos} 
+        onRemove={onRemove} 
+        onComplete={onComplete} />
+    </>
+  );
 }
 
 export default App;
