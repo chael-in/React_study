b0d05019af7c898a29ab9787d54a75a1918e815d
chael-in <cofls3511@gmail.com>
2025-04-10 23:30:24 +0900

    자기소개카드

---
+++ 3주차 과제/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+        <Card
+            name = "백채린"
+            age = "24"
+            hobby = "독서."
+        />
+    </div>
+  );
+}
+
+export default App;
